519ad3493d5401d45996de157d955609f0a9dc97
Nick Nist <nicknistoc@gmail.com>
2020-02-04 15:38:40 -0700

    Add ColorContainer to hold the 5 ColorPicker Components

---
+++ frontend/color-react/src/Components/ColorContainer/ColorContainer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ColorPicker from '../ColorPicker/ColorPicker';
+
+const ColorContainer = () => {
+  return (
+    <section className='container'>
+      <ColorPicker />
+      <ColorPicker />
+      <ColorPicker />
+      <ColorPicker />
+      <ColorPicker />
+    </section>
+  )
+}
+
+export default ColorContainer;
